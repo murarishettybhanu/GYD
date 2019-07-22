0cc462ab2570d4533cd144521f2b7f50a55820e3
Murarishetty Bhanu Teja <murarishettybhanuteja@gmail.com>
2019-07-23 01:42:22 +0530

    GYD 23/7/19

---
+++ database/models/User.js
@@ -0,0 +1,38 @@
+const bcrypt = require('bcryptjs')
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, 'Please provide your username']
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: [true, 'Please provide your email.']
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide your password.']
+  },
+  weightinkg: String,
+  heightincm:String,
+  age: String,
+  gender: String,
+  activity: String,
+  bmi: String,
+  bmr: String,
+  dailyCalories: String,
+  category:String
+})
+
+UserSchema.pre('save', function(next) {
+  const user = this
+
+  bcrypt.hash(user.password, 10, function (error, encrypted) {
+    user.password = encrypted
+    next()
+  })
+})
+
+module.exports = mongoose.model('User', UserSchema)
